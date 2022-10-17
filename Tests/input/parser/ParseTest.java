7698dcf80e18a583bea68cbbc767543fe87f0320
sastahl28 <112001400+sastahl28@users.noreply.github.com>
2022-10-17 00:36:07 -0400

    Test for geometry builder

---
+++ Tests/input/parser/ParseTest.java
@@ -0,0 +1,160 @@
+package input.parser;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import input.components.ComponentNode;
+import input.components.FigureNode;
+import input.exception.ParseException;
+
+
+
+
+/**
+ * 
+ * A test class to test capability of parse and unparse methods
+ * 
+ * @author Nick, James, Sally
+ * @Date 10-5-22
+ */
+
+class ParseTest
+
+
+{
+	
+	
+
+	/*
+	 * 
+	 * Test method to run the test on the parse method
+	 */
+	
+	public static ComponentNode runFigureParseTest(String filename)
+	{
+		JSONParser parser = new JSONParser();
+
+		String figureStr = utilities.io.FileUtilities.readFileFilterComments(filename);
+		
+		return parser.parse(figureStr);
+	}
+	
+	
+
+	/*
+	 * 
+	 * Test method to ensure functionality with a empty file
+	 */
+	
+	@Test
+	void empty_json_string_test()
+	{
+		JSONParser parser = new JSONParser();
+
+		assertThrows(ParseException.class, () -> { parser.parse("{}"); });
+	}
+
+	
+
+	/*
+	 * 
+	 * Test method to ensure functionality with a json file
+	 */
+	
+	@Test
+	void single_triangle_test()
+	{
+		ComponentNode node = ParseTest.runFigureParseTest("single_triangle.json");
+
+		assertTrue(node instanceof FigureNode);
+		
+		StringBuilder sb = new StringBuilder();
+		
+
+		node.unparse(sb, 1);
+		System.out.println(sb.toString());
+
+
+	}
+	
+	
+
+	/*
+	 * 
+	 * Test method to ensure functionality with a json file
+	 */
+	
+	@Test
+	void collinear_line_segments_test()
+	{
+		ComponentNode node = ParseTest.runFigureParseTest("collinear_line_segments.json");
+
+		assertTrue(node instanceof FigureNode);
+		
+		StringBuilder sb = new StringBuilder();
+
+
+		node.unparse(sb, 1);
+
+		System.out.println(sb.toString());
+
+	}
+	
+	
+
+	/*
+	 * 
+	 * Test method to ensure functionality with a json file
+	 */
+	
+	
+	
+	@Test
+	void crossing_symmetric_triangle_test()
+	{
+		ComponentNode node = ParseTest.runFigureParseTest("crossing_symmetric_triangle.json");
+
+		assertTrue(node instanceof FigureNode);
+		
+		StringBuilder sb = new StringBuilder();
+
+		
+		node.unparse(sb, 1);
+
+		System.out.println(sb.toString());
+
+	}
+	
+	
+	/*
+	 * 
+	 * Test method to ensure functionality with a json file
+	 */
+	
+	
+	@Test
+	void fully_connected_irregular_polygon_test()
+	{
+		ComponentNode node = ParseTest.runFigureParseTest("fully_connected_irregular_polygon.json");
+
+		assertTrue(node instanceof FigureNode);
+		
+		StringBuilder sb = new StringBuilder();
+		
+
+		node.unparse(sb, 1);
+		
+		System.out.println(sb.toString());
+
+
+	}
+	
+	
+	
+	
+	
+	
+	
+}
