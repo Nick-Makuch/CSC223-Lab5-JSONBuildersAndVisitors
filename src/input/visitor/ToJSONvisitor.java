92eeec1bca7652cfc3d978ea6a71d6a327f3750f
Nick-Makuch <nichols.makuch@gmail.com>
2022-10-16 22:27:18 -0400

    ToJSONvisitor started

--- src/input/visitor/ToJSONvisitor.java
+++ src/input/visitor/ToJSONvisitor.java
@@ -1,5 +1,26 @@
 package input.visitor;
 
-public class ToJSONvisitor {
+import org.json.JSONObject;
 
+import input.components.FigureNode;
+
+public class ToJSONvisitor 
+{
+	
+	//private JSONObject _JsonObject;
+	
+	public JSONObject toJsonObject(FigureNode node) 
+	{
+		JSONObject _JsonObject = new JSONObject();
+		
+		//TODO
+		
+		return _JsonObject;
+	}
+	
+	public String toString(int i) 
+	{
+		String jSONString = "";
+		return jSONString;
+	}
 }
