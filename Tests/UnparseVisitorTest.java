e9b1a847238e8a9b8aa84f746e003277498806f6
Nick-Makuch <nichols.makuch@gmail.com>
2022-10-17 00:53:44 -0400

    Updated UnparseVisitorTest
    Doesn't work but idk why

--- Tests/UnparseVisitorTest.java
+++ Tests/UnparseVisitorTest.java
@@ -37,6 +37,45 @@ public class UnparseVisitorTest
 		testSegmentNodeDatabase.addUndirectedEdge(_testNode4, _testNode1);
 		
 		FigureNode testFigureNode = new FigureNode(_description, _testPointNodeDatabase, testSegmentNodeDatabase);
+		
+		StringBuilder sb = new StringBuilder();
+		UnparseVisitor unparser = new UnparseVisitor();
+		
+		assertEquals("Description: description\r\n"
+				+ "Points:\r\n"
+				+ "{\r\n"
+				+ "    Point(Node1)(1.0, 1.0)\r\n"
+				+ "    Point(Node2)(2.0, 2.0)\r\n"
+				+ "    Point(Node3)(3.0, 3.0)\r\n"
+				+ "    Point(Node4)(4.0, 4.0)\r\n"
+				+ "}\r\n"
+				+ "Segments:\r\n"
+				+ "{\r\n"
+				+ "    Node2 : Node1 Node3 \r\n"
+				+ "    Node1 : Node2 Node4 \r\n"
+				+ "    Node3 : Node2 Node4 \r\n"
+				+ "    Node4 : Node3 Node1 \r\n"
+				+ "}", unparser.visitFigureNode(testFigureNode,
+		new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)).toString());
+		
+//		System.out.println(unparser.visitFigureNode(testFigureNode,
+//		new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)));
+//		
+//		Description: description
+//		Points:
+//		{
+//		    Point(Node1)(1.0, 1.0)
+//		    Point(Node2)(2.0, 2.0)
+//		    Point(Node3)(3.0, 3.0)
+//		    Point(Node4)(4.0, 4.0)
+//		}
+//		Segments:
+//		{
+//		    Node2 : Node1 Node3 
+//		    Node1 : Node2 Node4 
+//		    Node3 : Node2 Node4 
+//		    Node4 : Node3 Node1 
+//		}
 	}
 	
 	@Test
@@ -48,6 +87,30 @@ public class UnparseVisitorTest
 		testSegmentNodeDatabase.addUndirectedEdge(_testNode4, _testNode3);
 		testSegmentNodeDatabase.addUndirectedEdge(_testNode4, _testNode1);
 		
+		StringBuilder sb = new StringBuilder();
+		UnparseVisitor unparser = new UnparseVisitor();
+		
+		assertEquals("Segments:\r\n"
+				+ "{\r\n"
+				+ "    Node2 : Node1 Node3 \r\n"
+				+ "    Node1 : Node2 Node4 \r\n"
+				+ "    Node3 : Node2 Node4 \r\n"
+				+ "    Node4 : Node3 Node1 \r\n"
+				+ "}\r\n"
+				, unparser.visitSegmentDatabaseNode(testSegmentNodeDatabase,
+						new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)));
+		
+//		System.out.println(unparser.visitSegmentDatabaseNode(testSegmentNodeDatabase,
+//				new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)));
+//		
+//		Segments:
+//		{
+//		    Node2 : Node1 Node3 
+//		    Node1 : Node2 Node4 
+//		    Node3 : Node2 Node4 
+//		    Node4 : Node3 Node1 
+//		}
+
 	}
 	
 	@Test
@@ -58,6 +121,29 @@ public class UnparseVisitorTest
 		_testPointNodeDatabase.put(_testNode2);
 		_testPointNodeDatabase.put(_testNode3);
 		_testPointNodeDatabase.put(_testNode4);
+		
+		StringBuilder sb = new StringBuilder();
+		UnparseVisitor unparser = new UnparseVisitor();
+		
+		assertEquals("Points:\r\n"
+				+ "	{\r\n"
+				+ "    Point(Node1)(1.0, 1.0)\r\n"
+				+ "	   Point(Node2)(2.0, 2.0)\r\n"
+				+ "	   Point(Node3)(3.0, 3.0)\r\n"
+				+ "	   Point(Node4)(4.0, 4.0)\r\n"
+				+ "	}" + "\n", unparser.visitPointNodeDatabase(_testPointNodeDatabase,
+						new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)).toString());
+		
+//		System.out.println(unparser.visitPointNodeDatabase(_testPointNodeDatabase,
+//				new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)));
+		
+//		Points:
+//		{
+//		    Point(Node1)(1.0, 1.0)
+//		    Point(Node2)(2.0, 2.0)
+//		    Point(Node3)(3.0, 3.0)
+//		    Point(Node4)(4.0, 4.0)
+//		}
 	}
 	
 	@Test
@@ -69,13 +155,19 @@ public class UnparseVisitorTest
 		assertEquals("(Node1)(1.0, 1.0)",
 				unparser.visitPointNode(_testNode1, new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)).toString());
 		
-		assertEquals("(Node1)(2.0, 2.0)",
+		sb.delete(0, sb.length());
+		
+		assertEquals("(Node2)(2.0, 2.0)",
 				unparser.visitPointNode(_testNode2, new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)).toString());
 		
-		assertEquals("(Node1)(3.0, 3.0)",
+		sb.delete(0, sb.length());
+		
+		assertEquals("(Node3)(3.0, 3.0)",
 				unparser.visitPointNode(_testNode3, new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)).toString());
 		
-		assertEquals("(Node1)(4.0, 4.0)",
+		sb.delete(0, sb.length());
+		
+		assertEquals("(Node4)(4.0, 4.0)",
 				unparser.visitPointNode(_testNode4, new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)).toString());
 		
 	}
