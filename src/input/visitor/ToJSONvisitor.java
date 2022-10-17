babccad12c6bca147203526bc79b7369173bfdb7
JamesC115 <112016529+JamesC115@users.noreply.github.com>
2022-10-16 23:10:51 -0400

    tojsonvisitor constructor

--- src/input/visitor/ToJSONvisitor.java
+++ src/input/visitor/ToJSONvisitor.java
@@ -1,26 +1,37 @@
 package input.visitor;
 
+import java.util.AbstractMap;
+
 import org.json.JSONObject;
 
 import input.components.FigureNode;
 
 public class ToJSONvisitor 
 {
+	JSONObject _JsonObject;
 	
-	//private JSONObject _JsonObject;
+	public ToJSONvisitor(FigureNode node){
+		StringBuilder sb = new StringBuilder();
+		UnparseVisitor unparser = new UnparseVisitor();
+		_JsonObject = (JSONObject) unparser.visitFigureNode((FigureNode)node,
+		 new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0));
+	}
 	
+	/*
 	public JSONObject toJsonObject(FigureNode node) 
 	{
-		JSONObject _JsonObject = new JSONObject();
+		StringBuilder sb = new StringBuilder();
+		UnparseVisitor unparser = new UnparseVisitor();
+		return (JSONObject) unparser.visitFigureNode((FigureNode)node,
+		 new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0));
+
 		
 		//TODO
 		
-		return _JsonObject;
 	}
-	
+	*/
 	public String toString(int i) 
 	{
-		String jSONString = "";
-		return jSONString;
+		return _JsonObject.toString(i);
 	}
 }
