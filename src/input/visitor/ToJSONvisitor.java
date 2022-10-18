6902f1e41947405fcda1ae3295ea2c35d61cd25e
Nick-Makuch <nichols.makuch@gmail.com>
2022-10-18 11:33:40 -0400

    Checked

--- src/input/visitor/ToJSONvisitor.java
+++ src/input/visitor/ToJSONvisitor.java
@@ -13,66 +13,7 @@ import input.components.segment.SegmentNodeDatabase;
 import utilities.io.StringUtilities;
 
 public class ToJSONvisitor implements ComponentNodeVisitor
-{
-//	JSONObject _JsonObject;
-//	
-//	public ToJSONvisitor(FigureNode node){
-//		StringBuilder sb = new StringBuilder();
-//		UnparseVisitor unparser = new UnparseVisitor();
-	
-		//dont call unparser here
-	
-//		System.out.println(unparser.visitFigureNode((FigureNode)node,
-//				 new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)).toString());
-//		_JsonObject = new JSONObject(unparser.visitFigureNode((FigureNode)node,
-//		 new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0)).toString());
-//	}
-//	
-//	public JSONObject toJsonObject(FigureNode node) 
-//	{
-//		StringBuilder sb = new StringBuilder();
-//		UnparseVisitor unparser = new UnparseVisitor();
-//		return (JSONObject) unparser.visitFigureNode((FigureNode)node,
-//		 new AbstractMap.SimpleEntry<StringBuilder, Integer>(sb, 0));		
-//	}
-//	
-//	public String toString(int i) 
-//	{
-//		return _JsonObject.toString(i);
-//	}
-
-//	@Override
-//	public Object visitFigureNode(FigureNode node, Object o) 
-//	{		
-//		// Unpack the input object containing a Stringbuilder and an indentation level
-//		@SuppressWarnings("unchecked")
-//		AbstractMap.SimpleEntry<StringBuilder, Integer> pair = (AbstractMap.SimpleEntry<StringBuilder, Integer>)(o);
-//		StringBuilder sb = pair.getKey();
-//		int level = pair.getValue();
-//		//UnparseVisitor unparser = new UnparseVisitor();
-//			
-//		sb.append(StringUtilities.indent(level) + "{" + "\n");
-//		pair.setValue(pair.getValue() + 2);
-//				
-//		sb.append(StringUtilities.indent(level+1) + "Figure:\n");
-//		sb.append(StringUtilities.indent(level+1) +"{" + "\n");
-//		sb.append(StringUtilities.indent(level+2)+ "Description: " + node.getDescription() + ",\n");
-//		        
-////		node.getPointsDatabase().accept(unparser, o);
-////		node.getSegments().accept(unparser, o);
-////		sb.append(unparser.visitPointNodeDatabase(node.getPointsDatabase(), o));
-//		        
-//		sb.append(StringUtilities.indent(level+1) + "}\n");
-//		sb.append(StringUtilities.indent(level) + "}\n");
-//		
-//		return new JSONObject(sb.toString());
-//		//return new JSONObject(new JSONTokener(sb.toString()));
-//		
-////		return new JSONObject(node.toString());
-//		
-//		//return null;
-//	}
-	
+{	
 	/**
 	 * Converts provided FigureNode into a JSONobject
 	 * @return object (JSONobject)
@@ -117,17 +58,7 @@ public class ToJSONvisitor implements ComponentNodeVisitor
 	 * */
 	@Override
 	public Object visitSegmentDatabaseNode(SegmentNodeDatabase node, Object o) 
-	{
-		// Unpack the input object containing a Stringbuilder and an indentation level
-//		@SuppressWarnings("unchecked")
-//		AbstractMap.SimpleEntry<StringBuilder, Integer> pair = (AbstractMap.SimpleEntry<StringBuilder, Integer>)(o);
-//		StringBuilder sb = pair.getKey();
-//		int level = pair.getValue();
-//		        
-//		node.unparse(sb, level);
-//		
-//		return new JSONObject(sb.toString());
-		
+	{		
 		JSONObject jsonSegNodeDatabase = new JSONObject();
 		StringBuilder sb = new StringBuilder();
 		int level = 0;
